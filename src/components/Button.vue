0ef60c00487e5a9d942be232de91f7f9f0213e4d
XiomaraBarreto <xiomarabarreto69@gmail.com>
2023-06-27 18:08:20 -0300

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<script setup>
+    const props = defineProps({
+        operador: String,
+        fn: Function
+    });
+
+    console.log(props);
+</script>
+
+<template>
+    <button
+        type="button"
+        class="h-10 w-10 flex items-center justify-center font-bold bg-violet-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-violet-500 text-white text-2xl"
+        @click="$emit('fn')"
+    >{{ operador }}</button>
+</template>
